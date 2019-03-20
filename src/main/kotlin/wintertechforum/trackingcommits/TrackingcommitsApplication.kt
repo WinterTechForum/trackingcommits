d61faa5f731ceca20aa521a48a778eb85b3bb91b
Justin Ryan <jryan@netflix.com>
2019-03-20 14:54:00 -0600

    Initial Commit

---
+++ src/main/kotlin/wintertechforum/trackingcommits/TrackingcommitsApplication.kt
@@ -0,0 +1,11 @@
+package wintertechforum.trackingcommits
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TrackingcommitsApplication
+
+fun main(args: Array<String>) {
+	runApplication<TrackingcommitsApplication>(*args)
+}
